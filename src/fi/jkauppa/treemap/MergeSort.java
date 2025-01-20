77d132e4465ee4c8ee84d23bdd233eb437abf6ae
goofyseeker311 <goofyseeker311@gmail.com>
2025-01-21 00:14:15 +0200

    Update MergeSort.java
    Partial update.

--- src/fi/jkauppa/treemap/MergeSort.java
+++ src/fi/jkauppa/treemap/MergeSort.java
@@ -7,22 +7,29 @@ public class MergeSort<T> {
 		int maxstep = (int)Math.ceil(Math.log(array.length) / Math.log(2));
 		System.out.println("maxstep: "+maxstep);
 		for (int k=0;k<maxstep;k++) {
-			int seg = (int)Math.pow(2, k);
-			int seg2 = (int)Math.pow(2, k+1);
-			int sc = (int)Math.pow(2, maxstep-(k+1));
-			int sc2 = (int)Math.pow(2, maxstep-k);
+			int seg = 1<<k;
+			int seg2 = 1<<(k+1);
+			int sc = 1<<(maxstep-(k+1));
+			int sc2 = 1<<(maxstep-k);
 			System.out.println("seg: "+seg+", seg2: "+seg2+", sc: "+sc+", sc2: "+sc2);
 			for (int j=0;j<sc;j++) {
 				int p1 = j*seg2;
 				int p2 = j*seg2+seg;
 				int p = p1;
 				int pm = p+seg2;
-				if (pm>array.length) {
-					pm = array.length;
-				}
-				System.out.println("j: "+j+" p1: "+p1+" p2: "+p2);
+				if (pm>array.length) {pm = array.length;}
+				System.out.print("j: "+j+", p1: "+p1+", p2: "+p2+", val:");
+				Comparable<T> tmp = null;
 				for (int i=p;i<pm;i++) {
+					Comparable<T> tmpv = array[i];
+					Comparable<T> p1v = null;
+					Comparable<T> p2v = null;
+					if (p1<array.length) {p1v=array[p1];}
+					if (p2<array.length) {p2v=array[p2];}
+					tmp = tmpv;
+					System.out.print(" "+array[i]);
 				}
+				System.out.println();
 			}
 		}
 	}
