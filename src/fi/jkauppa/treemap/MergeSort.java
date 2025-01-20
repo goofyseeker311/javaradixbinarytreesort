e4617307aa1d1fe65c86b2ef93869e860306c840
goofyseeker311 <goofyseeker311@gmail.com>
2025-01-20 20:28:23 +0200

    Update MergeSort.java
    Partial update to the in-array merge sort algorithm.

--- src/fi/jkauppa/treemap/MergeSort.java
+++ src/fi/jkauppa/treemap/MergeSort.java
@@ -6,9 +6,17 @@ public class MergeSort<T> {
 		Comparable<T>[] array = (Comparable<T>[]) data;
 		int maxstep = (int)Math.ceil(Math.log(array.length) / Math.log(2));
 		System.out.println("maxstep: "+maxstep);
-		for (int b=0;b<maxstep;b++) {
-			int seg = (int)Math.pow(2, b);
-			System.out.println("seg: "+seg);
+		for (int j=0;j<maxstep;j++) {
+			int seg = (int)Math.pow(2, j);
+			int seg2 = (int)Math.pow(2, j+1);
+			int sc = (int)Math.pow(2, maxstep-(j+1));
+			int sc2 = (int)Math.pow(2, maxstep-j);
+			System.out.println("seg: "+seg+", seg2: "+seg2+", sc: "+sc+", sc2: "+sc2);
+			for (int i=0;i<sc;i++) {
+				int p1 = i*seg2;
+				int p2 = i*seg2+seg;
+				System.out.println("i: "+i+" p1: "+p1+" p2: "+p2);
+			}
 		}
 	}
 }
